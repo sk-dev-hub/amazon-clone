7dc73b0db9abce0712dd31922a7a44310ce59539
sk-dev-hub <yolloboom@gmail.com>
2023-01-14 11:56:43 +0300

    step1

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,17 +1,299 @@
 <script setup>
 import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import MapMarkerOutlineIcon from 'vue-material-design-icons/MapMarkerOutline.vue';
+import MenuDownIcon from 'vue-material-design-icons/MenuDown.vue';
+import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
+import CartMinusIcon from 'vue-material-design-icons/CartMinus.vue';
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
+import AccountCircleIcon from 'vue-material-design-icons/AccountCircle.vue';
+import CloseIcon from 'vue-material-design-icons/Close.vue';
+import ChevronRightIcon from 'vue-material-design-icons/ChevronRight.vue';
+
+
 import { Link } from '@inertiajs/inertia-vue3';
 
-const showingNavigationDropdown = ref(false);
+let showMenu = ref(false)
+let accountAndList = ref(false)
+
+const accountAndListFunc = (bool) => {
+    setTimeout(() => {
+        accountAndList.value = bool
+    }, 150);
+}
 </script>
 
 <template>
-    <div>
-        cool
+    <div class="min-w-[1150px] bg-gray-100 h-full">
+
+        <div v-if="accountAndList" class="top-0 z-20 fixed w-full h-full bg-black bg-opacity-70"></div>
+        <div class="flex items-center bg-gray-900 h-[60px] py-2 fixed z-50 min-w-[1150px] w-full">
+            
+            <div class="flex">
+                <Link class="text-white h-[50px] p-2 pt-3 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    AMAZON
+                </Link>
+            </div>
+        
+            <div class="text-white h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                <div class="flex items-center justify-center">
+                    <MapMarkerOutlineIcon class="pt-2 -ml-1" fillColor="#f5f5f5" />
+
+                    <div>
+                        <div class="text-[13px] text-gray-300 font-extrabold ">
+                            <div>Delivery to John</div>
+                        </div>
+
+                        <div class="text-[15px] text-white -mt-1.5 font-extrabold ">
+                            <div>London SW2 SW2</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="flex grow items-center h-[45px] px-1">
+                <div class="flex items-center justify-center bg-gray-100 border-r border-r-gray-300 font-extrabold text-[11px] text-gray-600 w-[60px] h-[40px] rounded-l-md cursor-pointer">
+                    <div class="pt-[3px]">
+                        All
+                    </div>
+                    <MenuDownIcon class="-mr-2" fillColor="#5E5E5E" :size="20" />
+                </div>
+                <input type="text" class="block w-full border-none border-transparent focus:border-transparent focus:ring-0">
+                <div class="cursor-pointer bg-orange-300 p-[5px] px-2 rounded-r-md">
+                    <MagnifyIcon :size="30" />
+                </div>    
+            </div>
+
+            <div class="flex">
+                <div class="h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-center mt-2.5 px-1">
+                        <img src="https://via.placeholder.com/35x20" alt="">
+                        <div class="text-[15px] text-white -mt-2 -mr-0.5 font-extrabold">EN</div>
+                        <MenuDownIcon class="-mr-4 -mt-1.5 pr-1" fillColor="#c2c2c2" :size="20" />
+                    </div>
+                </div>
+
+                <div @mouseenter="accountAndListFunc(true)" 
+                     @mouseleave="accountAndListFunc(false)"          
+                    class="h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-center">
+                        <div>
+                            <div class="text-[12px] text-white font-extrabold">
+                                Hello,
+                                <span>sign in</span>
+                            </div>
+                            <div class="flex items-center">
+                                <div class="text-[15px] text-white -mt-1.5 font-extrabold">Account & List</div>
+                                <MenuDownIcon class="-mr-4 -mt-1 pr-1" fillColor="#c2c2c2" :size="20" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div v-if="accountAndList" class="bg-white absolute z-50 top-[56px] -ml-[230px] w-[480px] rounded-sm px-6">
+                        <div>
+                            <div class="flex items-center justify-between py-2.5 border-b">
+                                <div class="text-sm p-2">Who's shopping? Select a profile.</div>
+                                <div class="flex items-center text-sm fond bold text-teal-600 hover:text-red-600 hover:underline">
+                                    Manage profile
+                                    <ChevronRightIcon :size="20" fillColor="#808080" />
+                                </div>
+                            </div>
+
+                            <div class="flex">
+                                <div class="w-1/2 border-r">
+                                    <div class="pb-3">
+                                        <div class="font-extrabold pt-3">Your List</div>
+                                        <div class="text-sm hover:text-red-600 hover:underline pt-3">Create a List</div>
+
+                                    </div>
+                                </div>
+                                <div class="w-1/2 ml-5">
+                                    <div class="pb-3">
+                                        <div class="font-extrabold pt-3">Your Account</div>
+                                        <div class="text-sm hover:text-red-600 hover:underline pt-3">Account</div>
+                                        <div class="text-sm hover:text-red-600 hover:underline pt-3">Sign Out</div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-center">
+                        <div>
+                            <div class="text-[12px] text-white font-extrabold"> Returns </div>
+                            <div class="flex items-center">
+                                <div class="text-[15px] text-white -mt-1.5 font-extrabold">& Orders</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="relative h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <span class="absolute text-center right-[21px] w-[14px] -top-0 rounded-full text-[20px]">
+                        <div class="text-orange-400 font-extrabold bg-gray-900 h-[16px]">0</div>
+                    </span>
+                    <div class="flex items-center justify-center">
+                        <CartMinusIcon fillColor="#fcfcfc" :size="40" class="-mt-0.5" />
+                    </div>
+                </div>        
+            </div>
+        </div>
+
+        <div class="flex items-center justify-between bg-[#232f3e] h-[38px] fixed z-40 min-w-[1150px] w-full mt-[60px]">
+            <div class="flex">
+                <div @click="showMenu = true" class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <MenuIcon fillColor="#f5f5f5" :size="26" class="mr-0.5" />
+                        <div class="text-[14px] text-white font-extrabold">All</div>
+                    </div>
+                </div>
+                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <div class="text-[14px] text-white font-extrabold">Today's Deals</div>
+                    </div>
+                </div>
+                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <div class="text-[14px] text-white font-extrabold">Gift Cards</div>
+                    </div>
+                </div>
+                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <div class="text-[14px] text-white font-extrabold">Buy Again</div>
+                    </div>
+                </div>
+                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <div class="text-[14px] text-white font-extrabold">Customer Service</div>
+                    </div>
+                </div>
+                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <div class="text-[14px] text-white font-extrabold">Jons Amazon.com</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex">
+                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                    <div class="flex items-center justify-between px-2">
+                        <div class="text-[14px] text-white font-extrabold">Holiday Fashion Deals</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <main class="max-w-[1500px] mx-auto">
+            <div class="pt-[98px]"></div>
+            <slot />
+        </main>
+                   
+            <div class="w-full bg-white mt-10">
+                <div class="max-w-[1500px] mx-auto">
+                    <div class="text-[23px] pt-4 font-extrabold">Recommended based on your shopping trends</div>
+                    <div class="flex justify-center items-stretch">
+                        <div class="p-4 text-center mx-auto">
+                            <div class="w-[158px] h-[150px] overflow-hidden">
+                                <img src="https://via.placeholder.com/158x150" alt="">
+                            </div>
+                            <div class="w-[160px] text-[12px] py-2 text-teal-600 font-extrabold hover:text-red-600 cursor-pointer">
+                                This is a test title
+                            </div>
+                            <div class="flex justify-start">
+                                <div class="text-xs font-extrabold text-red-600 w-full text-left ">
+                                    $99.99
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <footer  class="bg-[#232f3e]">
+                <div class="bg-[#37475a] hover:bg-gray-600 w-full p-3.5 cursor-pointer text-white font-extrabold text-center">
+                    Back to top
+                </div>
+                <div class="flex items-stretch justify-between max-w-[1000px] mx-auto p-10 text-white">
+                    <ul>
+                        <li class="font-extrabold text-lg">Get to Know Us</li>
+                        <li class="hover:underline cursor-pointer">Careers</li>
+                        <li class="hover:underline cursor-pointer">About Amazon</li>
+                        <li class="hover:underline cursor-pointer">investor Relations</li>
+                        <li class="hover:underline cursor-pointer">Amazon Devices</li>
+                        <li class="hover:underline cursor-pointer">Amazon Science</li>
+                    </ul>
+
+                    <ul>
+                        <li class="font-extrabold text-lg">Make Money with us</li>
+                        <li class="hover:underline cursor-pointer">Sell products on Amazon</li>
+                        <li class="hover:underline cursor-pointer">Sell on Amazon Business</li>
+                        <li class="hover:underline cursor-pointer">Sell apps on Amazon</li>
+                        <li class="hover:underline cursor-pointer">Become am Affiliate</li>
+                        <li class="hover:underline cursor-pointer">Advertise Your Products</li>
+                        <li class="hover:underline cursor-pointer">Self-Publish with Us</li>
+                        <li class="hover:underline cursor-pointer">Host in Amazon Hub</li>
+                    </ul>
+
+                    <ul>
+                        <li class="font-extrabold text-lg">Get to Know Us</li>
+                        <li class="hover:underline cursor-pointer">Careers</li>
+                        <li class="hover:underline cursor-pointer">About Amazon</li>
+                        <li class="hover:underline cursor-pointer">investor Relations</li>
+                        <li class="hover:underline cursor-pointer">Amazon Devices</li>
+                        <li class="hover:underline cursor-pointer">Amazon Science</li>
+                    </ul>
+
+                    <ul>
+                        <li class="font-extrabold text-lg">Make Money with us</li>
+                        <li class="hover:underline cursor-pointer">Sell products on Amazon</li>
+                        <li class="hover:underline cursor-pointer">Sell on Amazon Business</li>
+                        <li class="hover:underline cursor-pointer">Sell apps on Amazon</li>
+                        <li class="hover:underline cursor-pointer">Become am Affiliate</li>
+                        <li class="hover:underline cursor-pointer">Advertise Your Products</li>
+                        <li class="hover:underline cursor-pointer">Self-Publish with Us</li>
+                        <li class="hover:underline cursor-pointer">Host in Amazon Hub</li>
+                    </ul>
+                
+                </div>
+            </footer>
+    </div>
+
+    <div 
+        v-if="showMenu"
+        class="top-0 z-50 fixed w-full h-full bg-black bg-opacity-70"
+        :class="[showMenu ? 'animate__animated animate__fadeIn animate__faster': '' ]"
+        >
+
+        <CloseIcon 
+            @click="showMenu = false"
+            :size="30"
+            fillColor="#dcdcdc"
+            class="ml-2.5 mt-3.5 left-80 cursor-pointer fixed z-50"
+            :class="[showMenu ? 'animate__animated animate__fadeIn animate__faster': '' ]"
+
+        />
+        <div class="w-80 h-full bg-white"
+            :class="[showMenu ? 'animate__animated animate__slideInLeft animate__faster': '' ]"
+        >
+            <div class="bg-[#232f3e] font-extrabold text-[18px] flex items-center p-2 text-white pl-7">
+                <span>Hello, Sign in</span>
+            </div>
+
+            <div class="font-extrabold text-[16px] pt-3 pb-1 pl-6 pr-3 text-black">
+                Shop by Department
+            </div>
+
+            <div class="hover:bg-gray-200 pl-6 pr-3">
+                <div class="py-2.5 text-[13px] text-black flex justify-between items-center hover:bg-gray-200 cursor-pointer">
+                    Computers <ChevronRightIcon :size="20" fillColor="#808080" />
+                </div>
+            </div>
+        </div>
+    
     </div>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,78 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Navigation, Slide} from 'vue3-carousel'
+
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+        <Carousel :items-to-show="1" :wrap-around="true">
+            <Slide v-for="slide in 10" :key="slide">
+            <div class="carousel__item">
+                <img src="https://via.placeholder.com/3000x1500.png/004466?text=Testing" alt="">
+            </div>
+            </Slide>
+
+            <template #addons>
+            <Navigation />
+            </template>
+        </Carousel>
+
+        <div class="relative -mt-[300px] ">
+            <div class="flex m-4 z-10 relative">
+                <div class="bg-white mx-2 p-2 text-md w-full text-center">
+                    You are on amazon.com You can also shop on Amazon UK for millions of products with fast local delivery.
+                    <span class="underline text-teal-600 cursor-pointer">Click here to go to amazon.co.uk</span>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-3 m-4 z-10 relative">
+                <div class="p-1.5 flex">
+                    <div class="bg-white p-5 ">
+                        <div class="text-2xl font-extrabold flex">Computers</div>
+                        <div class="flex">
+                            <img class="object-fill" src="https://via.placeholder.com/1097x756" alt="">
+                        </div>
+                        <div class="pt-3 -mb-2 text-teal-800 font-bold hover:underline hover:text-red-400 cursor-pointer">
+                            See More
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     </AuthenticatedLayout>
 </template>
+
+<style>
+.carousel__item {
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__prev,
+.carousel__next {
+    width: 40px;
+    height: 200px;
+    color: rgb(196, 196, 196);
+    margin-top: -100px;
+    border: transparent;
+}
+
+.carousel__prev:hover,
+.carousel__next:hover {
+    width: 40px;
+    height: 200px;
+    color: rgb(212, 212, 212);
+    margin-top: -100px;
+    border: 3px solid rgb(217, 217, 217) ;
+}
+</style>
